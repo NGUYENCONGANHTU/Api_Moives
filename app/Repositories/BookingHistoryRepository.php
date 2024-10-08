163791556ee0cf8e45926c30749915d9337ba659
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2024-10-08 17:49:01 +0700

    update reposition

---
+++ app/Repositories/BookingHistoryRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\BaseRepository;
+use App\Models\BookingHistory;  // import model tương ứng
+use Exception; 
+
+class BookingHistoryRepository extends BaseRepository
+{
+     /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return BookingHistory::class;
+    }
+
+    /**
+     * Get data by multiple fields
+     *
+     * @param array $params
+     * @return mixed
+     */
+    public function search($params)
+    {
+        // default limit
+        $limit = config('constant.defaultLimit');
+        $query = $this->model->query();
+
+        if (isset($params['name'])) {
+            $query->where('name', 'like', '%' . $params['name'] . '%');
+        }
+
+        if (isset($params['status'])) {
+            $query = $query->where('status', '=', (int) $params['status']);
+        }
+
+        return $query->paginate($limit);
+    }
+}
