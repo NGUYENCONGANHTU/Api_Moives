15cfa38bad7ebafd3bceaa5e5b41ffd3c2f572b2
Can Van Diet <diet.canvan@devfast.us>
2024-10-12 15:10:18 +0700

    update-base

--- routes/users.php
+++ routes/users.php
@@ -1,11 +1,6 @@
 <?php
 use App\Http\Controllers\UserAuthenticateController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderDetailController;
-use App\Http\Controllers\HistoryController;
-use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,35 +21,12 @@ Route::prefix('api/users')->group(function () {
 
 
     Route::get('home/banner', [HomeController::class, 'homeBanner'])->name('banner-home');
-    Route::get('home/category', [HomeController::class, 'homeCategory'])->name('category-home');
-    Route::get('home/trademark', [HomeController::class, 'homeTrademark'])->name('trademark-home');
-    Route::get('home/product_images', [HomeController::class, 'homeProductImages'])->name('product-images-home');
-    Route::get('home/product', [HomeController::class, 'homeProduct'])->name('product-home');
-    Route::get('home/product_detail/{id}', [HomeController::class, 'homeProductDetail'])->name('product-detail-home');
-    Route::put('home/put_product_heart/{id}', [HomeController::class, 'updateHeartProduct'])->name('heart-home-put');
-    Route::post('home/post_contact', [HomeController::class, 'homeStoreContact'])->name('post_contact');
     
     Route::group(['middleware' => ['jwt.verify', 'auth:app-users']], function () {
 
         Route::put('user_authenticate/change_password', [UserAuthenticateController::class, 'changePassword']);
         Route::get('user_authenticate/user_info/{id}', [UserAuthenticateController::class, 'userInfo']);
         Route::get('user_authenticate/logout', [UserAuthenticateController::class, 'logout']);
-        Route::put('home/delete_product_review', [HomeController::class, 'deleteProductReview'])->name('delete-product-review');
-        Route::get('home/product_review', [HomeController::class, 'homeProductReview'])->name('product-review-home');
-        Route::put('home/put_product_review/{id}', [HomeController::class, 'updateProductReview'])->name('put_product_review');
-        Route::post('home/post_product_review', [HomeController::class, 'createProductReview'])->name('post_product_review');
-        Route::delete('home/delete_product_review', [HomeController::class, 'deleteProductReview'])->name('delete_product_review');
-        Route::put('home/user_post_contact', [HomeController::class, 'homeStoreContact'])->name('user_post_contact');
-        Route::get('cart', [CartController::class, 'index'])->name('cart-user-index');
-        Route::post('cart/store/{productId}', [CartController::class, 'store'])->name('cart-user-store');
-        Route::put('cart/update/{productId}', [CartController::class, 'update'])->name('cart-user-update');
-        Route::delete('cart/destroy/{productId}', [CartController::class, 'destroy'])->name('cart-user-destroy');
-        Route::get('order', [OrderController::class, 'index'])->name('order-user-index');
-        Route::post('order/checkOut', [OrderController::class, 'checkOut'])->name('order-user-checkOut');
-        Route::get('orderDetail', [OrderDetailController::class, 'index'])->name('orderDetail-user-index');
-        Route::get('notification', [NotificationController::class, 'index'])->name('user-notification-index');
-        Route::get('history', [HistoryController::class, 'index'])->name('user-history-index');
-
     });
     
 });
